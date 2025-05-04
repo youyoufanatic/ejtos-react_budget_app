1176fcf6474b1ba24a706778acf565bd4c1854b3
youyoufanatic <youcefrehab10@gmail.com>
2025-05-04 23:05:16 +0100

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -28,7 +28,7 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
+        <td>${props.cost}</td>
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
@@ -36,5 +36,3 @@ const ExpenseItem = (props) => {
 };
 
 export default ExpenseItem;
-
-
