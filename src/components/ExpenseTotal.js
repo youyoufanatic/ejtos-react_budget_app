d2358d5d79158fe35600704e2394046d449cae56
youyoufanatic <youcefrehab10@gmail.com>
2025-05-04 23:05:51 +0100

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,3 +1,4 @@
+
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const ExpenseTotal = () => {
@@ -7,10 +8,8 @@ const ExpenseTotal = () => {
     }, 0);
     return (
         <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+            <span>Spent so far: ${totalExpenses}</span>
         </div>
     );
 };
 export default ExpenseTotal;
-
-
